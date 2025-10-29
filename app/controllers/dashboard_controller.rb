b461bdf12983a45b7a7126e82039fe7fe77bb38a
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2025-10-29 14:26:57 +1000

    Create partial for breadcrumbs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    breadcrumb "breadcrumb path 1", dashboard_index_path
+    breadcrumb "breadcrumb path 2", dashboard_index_path
+    breadcrumb "breadcrumb path 3", root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "dashboard#index"
+  resources :dashboard, only: [:index]
 
   mount(
     Flipper::UI.app do |builder|
